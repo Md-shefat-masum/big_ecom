361a1660181a5426185d0b1b1c3fc0f915d6f90a
Shifat Naznin Nipa <shifatnaznin11@gmail.com>
2021-08-26 00:41:49 +0600

    new updates

--- resources/views/frontend/frontend.blade.php
+++ resources/views/frontend/frontend.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Antomi - Electronics eCommerce HTML Template</title>
+    <title>Brandszone</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
