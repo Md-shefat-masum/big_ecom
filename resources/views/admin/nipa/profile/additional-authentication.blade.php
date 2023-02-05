08e679faeeb5335f10ab06f039a48d8bf83d02c7
Muhammad Sajidul islam <sajid285.me@gmail.com>
2023-02-05 10:53:08 +0600

    pushed to frontend

--- resources/views/admin/nipa/profile/additional-authentication.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin.layouts.admin')
-
-@section('content')
-
-
-@endsection
--- resources/views/frontend_old/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('frontend.frontend')
-@section('content')
-    @include('frontend.include.banner')
-    @include('frontend.include.add-banner')
-    @include('frontend.include.latest-product')
-    @include('frontend.include.categoris')
-@endsection
